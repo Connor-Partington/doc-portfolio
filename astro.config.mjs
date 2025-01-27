096a1e4f9d69993642f75293e9cf3b643c4e207a
Connor <119803598+Connor-Partington@users.noreply.github.com>
2025-01-27 15:36:52 +0900

    Updated setting for github repo base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://connor-partington.github.io',
-	base: 'doc-portfolio',
+	base: '/doc-portfolio',
 	integrations: [
 		starlight({
 			title: 'Connor Partington',
